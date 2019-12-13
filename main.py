d645959c1ad4bb7acefdf2d1702f902a7958c696
vipul02 <vipul.2.july@gmail.com>
2019-12-14 00:36:03 +0530

    server error resolved

--- main.py
+++ main.py
@@ -6,26 +6,20 @@ import re
 from PIL import Image
 from io import BytesIO
 from base64 import b64decode
+import requests
 
 app = Flask(__name__)
-
+url = 'http://3.86.229.155:8080/extract_date'
+		
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	return 'Goto: https://date-ocr.herokuapp.com/extract_date'
+	return 'Goto: /extract_date'
 
 @app.route('/extract_date', methods=['POST', 'GET'])
 def date():
 	if request.method == 'POST':
 		data = request.get_json(force=True)
 		base64_img = data["base_64_image_content"]
-		if type(base64_img == str):
-			img = re.search(r'base64,(.*)', base64_img).group(1)
-			img = Image.open(BytesIO(b64decode(img)))
-		elif type(base64_img == bytes):
-			img = Image.open(BytesIO(b64decode(base64_img)))
-		
-		txt = ProcessImage().img_to_string(img)
-		date = ExtractDate().extract_date(txt)
-		return {"date": date}
+		return requests.post(url, json={"base_64_image_content" : base64_img}).json()
 	else:
 		return 'Please POST base64 image data'
